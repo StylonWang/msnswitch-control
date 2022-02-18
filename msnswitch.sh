d40694d1f5cdfc639a0f5eb7b7065b814a900ed6
Stylon Wang <stylon.wang@amd.com>
2022-02-18 13:38:30 +0800

    Bug script with missing target and control passed into the URL

--- msnswitch.sh
+++ msnswitch.sh
@@ -8,9 +8,11 @@ PASSWORD=test
 
 if (( $# < 3 )); then
 	echo "Usage: $0 switch-ip-address switch-target 0|1"
+	echo "   switch-taget: 1 or 2 as indicated on the case"
+	echo "   0|1: relay turned off or on"
 	exit
 fi
 
 echo "Control switch $SWITCH_IP, target $SWITCH, control $CONTROL"
 
-curl "http://$SWITCH_IP/cgi-bin/control.cgi?user=$ACCOUNT&passwd=$PASSWORD&target=1&control=1" --http1.1  --verbose --header "Accept-Encoding: gzip, deflate" --header "Accept: */*"
+curl "http://$SWITCH_IP/cgi-bin/control.cgi?user=$ACCOUNT&passwd=$PASSWORD&target=$SWITCH&control=$CONTROL" --http1.1  --verbose --header "Accept-Encoding: gzip, deflate" --header "Accept: */*"
